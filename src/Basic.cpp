68829b10191bdf73ffc45b8d39f77f23cfd06f35
yma <yaomian1212@sjtu.edu.cn>
2025-12-01 22:28:49 +0800

    feat: Finish Basic.cpp

--- src/Basic.cpp
+++ src/Basic.cpp
@@ -8,6 +8,8 @@
 #include "Token.hpp"
 #include "utils/Error.hpp"
 
+void Help();
+
 int main() {
   Lexer lexer;
   Parser parser;
@@ -19,10 +21,70 @@ int main() {
       continue;
     }
     try {
-      // TODO: The main function.
+
+      bool executed = false;
+      TokenStream crt_TStr = lexer.tokenize(line);
+      if (crt_TStr.size() == 1) {
+        if (line == "RUN") {
+          executed = true;
+          program.run();
+        }
+        if (line == "LIST") {
+          executed = true;
+          program.list();
+        }
+        if (line == "CLEAR") {
+          executed = true;
+          program.clear();
+        }
+        if (line == "QUIT") {
+          executed = true;
+          return 0;
+        }
+        if (line == "HELP") {
+          executed = true;
+          void Help();
+        }
+      }
+      if (!executed) {
+        ParsedLine crt_PLine = parser.parseLine(crt_TStr, line);
+        if (crt_PLine.getLine()) {
+          // 如果有行号，返回true
+          int line_num = crt_PLine.getLine().value();
+          if (crt_PLine.getStatement() == nullptr) {
+            program.removeStmt(line_num);
+          }
+          else {
+            // 存入指令集。
+            program.addStmt(line_num, crt_PLine.getStatement());
+          }
+        }
+        else {  // 直接执行。
+          program.execute(crt_PLine.getStatement());
+        }
+      }
+
     } catch (const BasicError& e) {
       std::cout << e.message() << "\n";
     }
   }
   return 0;
+}
+
+
+void Help() {
+  std::cout << "Supported commands:" << std::endl;
+  std::cout << "<line> REM <comment> - Comment" << std::endl;
+  std::cout << "( <line> ) LET <var> = <expr> - Assign expression value to variable" << std::endl;
+  std::cout << "( <line> ) PRINT <expr> - Evaluate and print expression" << std::endl;
+  std::cout << "( <line> ) INPUT <var> - Read integer input and assign to variable" << std::endl;
+  std::cout << "<line> END - End program execution" << std::endl;
+  std::cout << "<line> GOTO <line> - Jump to specified line" << std::endl;
+  std::cout << "<line> IF <expr1> <op> <expr2> THEN <line> - Conditional jump" << std::endl;
+  std::cout << "<line> - Delete line" << std::endl;
+  std::cout << "RUN - Start program execution" << std::endl;
+  std::cout << "LIST - List all program lines" << std::endl;
+  std::cout << "CLEAR - Clear all program lines" << std::endl;
+  std::cout << "QUIT - Exit interpreter" << std::endl;
+  std::cout << "HELP - Print this help message" << std::endl;
 }
